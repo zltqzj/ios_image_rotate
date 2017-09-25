c252f5328b7b3bee44122307ea26ac66235ec976
zhaojian <zltqzj@163.com>
2017-09-25 18:06:19 +0800

    二值化~~

---
+++ Opencv_oc/ViewController.mm
@@ -0,0 +1,193 @@
+//
+//  ViewController.m
+//  Opencv_oc
+//
+//  Created by zhaojian on 9/13/17.
+//  Copyright © 2017 zhaojian. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BlocksKit.h"
+#import "UIImage+fixOrientation.h"
+#import "UIImagePickerController+BlocksKit.h"
+#import <opencv2/opencv.hpp>
+@interface ViewController () <UIImagePickerControllerDelegate>
+@property(strong,nonatomic) UIImagePickerController* picker;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [_btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+    
+ 
+}
+
+-(void)click{
+    _picker = [[UIImagePickerController alloc] init];
+    _picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    _picker.allowsEditing = YES;
+    
+   __weak typeof(self) weakSelf = self;
+    
+    _picker.bk_didFinishPickingMediaBlock = ^(UIImagePickerController *picker , NSDictionary *info){
+ 
+        NSDictionary* dict = [info objectForKey:UIImagePickerControllerMediaMetadata];
+        
+        weakSelf.label.text = [NSString stringWithFormat:@"%@",dict[@"Orientation"]];
+        UIImage *selectImage = [[info objectForKey:UIImagePickerControllerOriginalImage] fixOrientation];
+        
+        
+//        weakSelf.imageView.image = selectImage ;
+        cv::Mat matImage = [weakSelf  cvMatFromUIImage:selectImage];
+        cv::Mat matGrey;
+        cv::cvtColor(matImage, matGrey, CV_BGR2GRAY);  // 灰度
+        
+        cv::GaussianBlur(matGrey, matGrey, cv::Size(5,5), 1.2,1.2);  // 高斯滤波
+        
+        
+        cv::Mat matBinary;
+        IplImage grey = matGrey;
+        unsigned char* dataImage = (unsigned char*)grey.imageData;
+        int threshold = Otsu(dataImage, grey.width, grey.height);
+        cv::threshold(matGrey, matBinary, threshold, 255, cv::THRESH_BINARY);   // 二值化
+        
+        weakSelf.imageView.image = [weakSelf UIImageFromCVMat:matBinary];
+ 
+        NSLog(@"%ld",(long)weakSelf.imageView.image.imageOrientation);  // 0
+        weakSelf.label2.text = [NSString stringWithFormat:@"%ld",(long)weakSelf.imageView.image.imageOrientation];
+        [weakSelf.picker dismissViewControllerAnimated:YES completion:nil];
+        
+        
+    };
+    _picker.bk_didCancelBlock  = ^(UIImagePickerController *picker){
+         
+    };
+    [self presentViewController:_picker animated:YES completion:nil];
+}
+
+
+
+#pragma mark - opencv method
+- (cv::Mat)cvMatFromUIImage:(UIImage *)image
+{
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+    CGFloat cols = image.size.width;
+    CGFloat rows = image.size.height;
+    
+    cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels
+    
+    CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
+                                                    cols,                       // Width of bitmap
+                                                    rows,                       // Height of bitmap
+                                                    8,                          // Bits per component
+                                                    cvMat.step[0],              // Bytes per row
+                                                    colorSpace,                 // Colorspace
+                                                    kCGImageAlphaNoneSkipLast |
+                                                    kCGBitmapByteOrderDefault); // Bitmap info flags
+    
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+    CGContextRelease(contextRef);
+    CGColorSpaceRelease(colorSpace);
+    
+    return cvMat;
+}
+
+-(UIImage *)UIImageFromCVMat:(cv::Mat)cvMat
+{
+    NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize()*cvMat.total()];
+    CGColorSpaceRef colorSpace;
+    
+    if (cvMat.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    }
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+    
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(cvMat.cols,                                 //width
+                                        cvMat.rows,                                 //height
+                                        8,                                          //bits per component
+                                        8 * cvMat.elemSize(),                       //bits per pixel
+                                        cvMat.step[0],                            //bytesPerRow
+                                        colorSpace,                                 //colorspace
+                                        kCGImageAlphaNone|kCGBitmapByteOrderDefault,// bitmap info
+                                        provider,                                   //CGDataProviderRef
+                                        NULL,                                       //decode
+                                        false,                                      //should interpolate
+                                        kCGRenderingIntentDefault                   //intent
+                                        );
+    
+    
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    return finalImage;
+}
+
+
+
+#pragma mark - custom method
+
+// OSTU算法求出阈值
+int  Otsu(unsigned char* pGrayImg , int iWidth , int iHeight)
+{
+    if((pGrayImg==0)||(iWidth<=0)||(iHeight<=0))return -1;
+    int ihist[256];
+    int thresholdValue=0; // „–÷µ
+    int n, n1, n2 ;
+    double m1, m2, sum, csum, fmax, sb;
+    int i,j,k;
+    memset(ihist, 0, sizeof(ihist));
+    n=iHeight*iWidth;
+    sum = csum = 0.0;
+    fmax = -1.0;
+    n1 = 0;
+    for(i=0; i < iHeight; i++)
+    {
+        for(j=0; j < iWidth; j++)
+        {
+            ihist[*pGrayImg]++;
+            pGrayImg++;
+        }
+    }
+    pGrayImg -= n;
+    for (k=0; k <= 255; k++)
+    {
+        sum += (double) k * (double) ihist[k];
+    }
+    for (k=0; k <=255; k++)
+    {
+        n1 += ihist[k];
+        if(n1==0)continue;
+        n2 = n - n1;
+        if(n2==0)break;
+        csum += (double)k *ihist[k];
+        m1 = csum/n1;
+        m2 = (sum-csum)/n2;
+        sb = (double) n1 *(double) n2 *(m1 - m2) * (m1 - m2);
+        if (sb > fmax)
+        {
+            fmax = sb;
+            thresholdValue = k;
+        }
+    }
+    return(thresholdValue);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
